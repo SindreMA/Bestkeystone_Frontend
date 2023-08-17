6bd976010937fa21995bba3d195066ae5cde412d
Sindre Andreassen <sindrema@gmail.com>
2023-08-17 11:59:40 +0200

    New build

---
+++ dist/spa/assets/axios.3f6a2e53.js
@@ -0,0 +1,1 @@
+import{b as e,a as o}from"./index.d11fe415.js";const i=o.create({baseURL:"https://api.example.com"});var t=e(({app:a})=>{a.config.globalProperties.$axios=o,a.config.globalProperties.$api=i});export{i as api,t as default};
